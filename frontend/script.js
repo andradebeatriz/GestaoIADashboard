7a316f7dec01d679c6b28abbb7c27c6f0369ddea
Beatriz Andrade <beatrizandrade020409@gmail.com>
2025-09-07 21:55:13 -0300

    Segundo commmit

--- frontend/script.js
+++ frontend/script.js
@@ -3,16 +3,18 @@ async function carregarUsuarios() {
     const response = await fetch("http://localhost:3001/usuarios");
     const usuarios = await response.json();
 
-    const div = document.createElement("div");
-    div.innerHTML = "<h3>Usuários no sistema:</h3>";
+    const tbody = document.querySelector("#usuarios tbody");
+    tbody.innerHTML = "";
 
     usuarios.forEach(u => {
-      const p = document.createElement("p");
-      p.textContent = `${u.id} - ${u.nome} (${u.email})`;
-      div.appendChild(p);
+      const tr = document.createElement("tr");
+      tr.innerHTML = `
+        <td>${u.id}</td>
+        <td>${u.nome}</td>
+        <td>${u.email}</td>
+      `;
+      tbody.appendChild(tr);
     });
-
-    document.body.appendChild(div);
   } catch (error) {
     console.error("Erro ao carregar usuários:", error);
   }
